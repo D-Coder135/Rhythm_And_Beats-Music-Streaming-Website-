ebb11c7618c2c7e2f0af32fa72a4484e1481cce0
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2021-11-08 19:55:42 +0530

    adding javascript to music.html

---
+++ js/script.js
@@ -0,0 +1,105 @@
+console.log("Welcome to Beats and Rythm");
+
+//Initialize the variables
+let songIndex = 0;
+let audioElement = new Audio("../songs/1.mp3");
+let masterPlay = document.getElementById("masterPlay");
+let myProgressBar = document.getElementById("myProgressBar");
+let gif = document.getElementById("gif");
+let songItems = Array.from(document.getElementsByClassName("songItem"));
+
+let songs = [
+  {
+    songName: "Rythm - Beats and Rythm Official",
+    filePath: "../songs/1.mp3",
+    coverPath: "../images/1.jpg",
+  },
+  {
+    songName: "Imagine Dragons - Thunder",
+    filePath: "../songs/2.mp3",
+    coverPath: "../images/2.jpg",
+  },
+  {
+    songName: "Lean On - Major Lazer & Dj Shake ft. Mo",
+    filePath: "../songs/3.mp3",
+    coverPath: "../images/3.jpg",
+  },
+  {
+    songName: "Roar - Kary Perry",
+    filePath: "../songs/4.mp3",
+    coverPath: "../images/4.jpg",
+  },
+  {
+    songName: "Let Me Love You - ft. DJ Snake ",
+    filePath: "../songs/5.mp3",
+    coverPath: "../images/5.jpg",
+  },
+  {
+    songName: "DJ Snake - Magenta Riddim",
+    filePath: "../songs/6.mp3",
+    coverPath: "../images/6.jpg",
+  },
+  {
+    songName: "DJ Snake - Taki Taki ft. Selena Gomez, ...",
+    filePath: "../songs/7.mp3",
+    coverPath: "../images/7.jpg",
+  },
+  {
+    songName: "Ed Sheeran - Shape of You",
+    filePath: "../songs/8.mp3",
+    coverPath: "../images/8.jpg",
+  },
+];
+
+songItems.forEach((element, i) => {
+  element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+  element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+});
+
+//audioElement.play()
+
+//Update play/pause click
+masterPlay.addEventListener("click", () => {
+  if (audioElement.paused || audioElement.currentTime <= 0) {
+    audioElement.play();
+    masterPlay.classList.remove("fa-play-circle");
+    masterPlay.classList.add("fa-pause-circle");
+    gif.style.opacity = 1;
+  } else {
+    audioElement.pause();
+    masterPlay.classList.remove("fa-pause-circle");
+    masterPlay.classList.add("fa-play-circle");
+    gif.style.opacity = 0;
+  }
+});
+
+//Listen to Event
+audioElement.addEventListener("timeupdate", () => {
+  //Update Seekbar
+  progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+  myProgressBar.value = progress;
+});
+
+myProgressBar.addEventListener("change", () => {
+  audioElement.currentTime =
+    (myProgressBar.value * audioElement.duration) / 100;
+});
+
+const makeAllPlays = () => {
+  Array.from(document.getElementsByClassName("songItemPlay")).forEach(
+    (element) => {
+      element.classList.remove("fa-pause-circle");
+      element.classList.add("fa-play-circle");
+    }
+  );
+};
+
+Array.from(document.getElementsByClassName("songItemPlay")).forEach(
+  (element) => {
+    element.addEventListener("click", (e) => {
+      makeAllPlays();
+      e.target.classList.remove("fa-play-circle");
+      e.target.classList.add("fa-pause-circle");
+    });
+  }
+);
--- js/songs.js
+++
@@ -1,4 +0,0 @@
-"use-strict";
-
-const selectedSong = () =>
-  (document.getElementById("song").style.font = "large");
